5a0c022b5dcc633d7e476d33b25c8b6ba7c98f5a
Zach Delano <z.delano@outlook.com>
2022-09-05 21:37:49 -0600

    add rows to have some spacing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,6 +13,14 @@
 
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
+        <v-row
+          v-for="(message, idx) in messages"
+          v-bind:key="idx"
+        >
+          <v-chip>
+            {{ message }}
+          </v-chip>
+        </v-row>
 
         <!-- If using vue-router -->
         <router-view></router-view>
@@ -33,64 +41,31 @@
 
 <script lang='ts'>
 import { defineComponent } from 'vue'
-
+import { io, Socket } from 'socket.io-client'
 
 export default defineComponent({
   name: 'HelloWorld',
 
+  mounted() {
+    const vm = this
+    if (!this.socket) {
+      this.socket = io('ws://localhost:3000')
+        // .on('getId', id => {
+        // })
+        .on('sendChat', thing => {
+          vm.messages.push(thing[0])
+        })
+    }
+  },
   methods: {
     sendMessage(message: string) {
-      console.log(message)
+      this.socket?.emit('chat', [message])
     }
   },
   data () {
     return {
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Roadmap',
-          href: 'https://vuetifyjs.com/en/introduction/roadmap/',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      socket: null as Socket | null,
+      messages: [] as string[]
     }
   },
 })
